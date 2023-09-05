b2af93d39a4b72afa2e7cf8062e458565b3f8593
Axelle Maolé <axellexm@gmail.com>
2023-09-05 16:10:41 +0200

    commentaires pour mieux se retrouver dans le fichier js

--- assets/script.js
+++ assets/script.js
@@ -17,24 +17,27 @@ const slides = [
 	}
 ];
 
+/* Elements */
 const leftArrow = document.querySelector(".arrow_left");
 const rightArrow = document.querySelector(".arrow_right");
 const dots = document.querySelectorAll(".dot");
 
+
+/* Functions, arrows */
 leftArrow.addEventListener('click', myFunction);
 rightArrow.addEventListener('click', myFunction);
 
-
+/* EventListener, arrows */
 function myFunction() {
 	alert ("Hello World!");
   }
 
+
+
   
 /*
-
 leftArrow.addEventListener("click", (event) => {});
 rightArrow.addEventListener("click", (event) => {});
-
 
 function ///() {
   /// ;
