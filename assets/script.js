f67a9fc7f9f74b8ba08ac5e32de7e4288a065da0
Axelle Maolé <axellexm@gmail.com>
2023-09-19 15:41:43 +0200

    cleaning code

--- assets/script.js
+++ assets/script.js
@@ -25,14 +25,26 @@ const slides = [
 ];
 
 let currentSlide = 0;
+activeDot() // dot selected at first
+
+/* Function, dots */
+function activeDot() {
+    dots.forEach((dot, index) => { //"forEach", calls a function for each element in an array - 
+        if (index === currentSlide) {
+            dot.classList.add("dot_selected");
+        }
+    });
+}
+/* "forEach" goes through each bullet point and removes the "dot_selected" class.
+"dot_selected" class is added only to the bullet point corresponding to the current slide. */
 
 
-/* EventListener, arrows */
+/* EventListener + functions, arrows */
 leftArrow.addEventListener('click', showPrev);
 rightArrow.addEventListener('click', showNext);
 
 function showNext() {
-	currentSlide = (currentSlide + 1) % slides.length; //"%" used to manage the carousel loop
+	currentSlide = (currentSlide + 1) % slides.length; //"%" used to manage the carousel loop -
 	bannerText.innerHTML = slides[currentSlide].tagLine;
 	bannerSlide.src = "./assets/images/slideshow/" + slides[currentSlide].image;
 	activeDot();
@@ -46,58 +58,3 @@ function showPrev() {
 }
 
   console.log(showNext)
-  
-
-  
-/* 
-
-
-
-function show next : 
-	bannerSlide.src = "./assets/images/slideshow/" + slides[currentSlide].image;
-function show prev : 
- bannerSlide.src = "./assets/images/slideshow/" + slides[currentSlide].image;
-
-
-
-	///
-
-function reset() {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i];
-  }
-}
-
-function startSlide() {
-  reset();
-  slides[0];
-}
-
-leftArrow.addEventListener("click", function() {
-  if (currentSlide === 0) {
-    currentSlide = slides.length;
-  }
-  showPrev;
-});
-
-rightArrow.addEventListener("click", function() {
-  if (currentSlide === slides.length - 1) {
-    currentSlide = -1;
-  }
-  showNext();
-});
-
-function showPrev() {
-	reset();
-	slides[currentSlide - 1];
-	currentSlide--;
-  }
-  
-  function showNext() {
-	reset();
-	slides[currentSlide + 1];
-	currentSlide++;
-  }
-  
-*/ 
-
